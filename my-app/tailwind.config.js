f3c64c0ea8fb5b9d6423c6b2321de2dbd65fa10e
Anusha <anukokala48@gmail.com>
2025-01-28 18:06:18 -0600

    Events page

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'orange': '#E39775',
+      'white': '#ffffff',
+      'blue': '#336274',
+      'gray': '#EEEEEE',
+      'lightblue': '#5C9EAC',
+    
+      
+    },
     extend: {},
   },
   plugins: [],
