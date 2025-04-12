f161bf4c5c2b5a45abcd98e8afadfdc039bcd517
masorol <miasorola.yoshida@gmail.com>
2025-04-12 18:31:54 -0500

    cleaning files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,8 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Events from "./pages/Events";
 import Gallery from "./pages/Gallery";
-import Landing from "./pages/Landing";
 import Membership from "./pages/MemberShip";
 import Resources from "./pages/Resources";
-import Events2 from "./pages/Events/Events";
 
 function App() {
   return (
@@ -21,9 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/Events" element={<Events />} />
-        <Route path="/Events2" element={<Events2 />} />
         <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/Landing" element={<Landing />} />
         <Route path="/Membership" element={<Membership />} />
         <Route path="/Resources" element={<Resources />} />
       </Routes>
--- my-app/src/pages/AboutUs.js
+++ my-app/src/pages/AboutUs.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { motion } from "framer-motion";
-import bgPhoto from "../assets/bgPhoto.jpg";
 const AboutUs = () => {
   return (
     <>
